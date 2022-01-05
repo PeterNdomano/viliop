b9d7189d30cbe343cdda43e25c4167d452bb4e82
PeterNdomano <peterndomano@gmail.com>
2022-01-05 15:13:15 +0300

    finalizing user functions

--- api/classes/User.php
+++ api/classes/User.php
@@ -20,11 +20,11 @@ class User
     if($userData !== false){
       $otp = rand(123456, 987654);
       $otpExpiry = time() + (30 * 60) //expires after 30 mins
-
+      $sql = $conn->prepare("UPDATE users SET otp = ?, otpExpiry = ? WHERE id = ?");
+      $sql->bind_param('sss', $otp, $otpExpiry, $id);
+      $sql->execute();
     }
-    $sql = $conn->prepare("UPDATE users SET otp = ?, otpExpiry = ? WHERE id = ?");
-    $sql->bind_param('sss', $otp, $otpExpiry, $id);
-    $sql->execute();
+
 
   }
 
@@ -40,6 +40,41 @@ class User
       }
     }
     return false;
+  }
+
+  public static function login($email, $password){
+    global $conn;
+    $ffo = array(
+      'status' => 0,
+      'msg' => 'Immature function termination',
+    );
+
+    $userData = self::getUserData($email);
+    if($userData !== false){
+      if(password_verify($password, $userData['password'])){
+        startSession();
+        $_SESSION['userId'] = $sql->insert_id;
+        $_SESSION['userToken'] = $token;
+        $ffo = array(
+          'status' => 1,
+          'msg' => 'Successful login',
+        );
+      }
+      else{
+        $ffo = array(
+          'status' => 0,
+          'msg' => 'Invalid login details',
+        );
+      }
+    }
+    else{
+      $ffo = array(
+        'status' => 0,
+        'msg' => 'Invalid login details',
+      );
+    }
+
+    return $ffo;
   }
 
   public static function register($email, $password){
@@ -63,7 +98,10 @@ class User
         startSession();
         $_SESSION['userId'] = $sql->insert_id;
         $_SESSION['userToken'] = $token;
-
+        $ffo = array(
+          'status' => 1,
+          'msg' => 'Successful registration',
+        );
       }
       else{
         $ffo = array(
@@ -83,6 +121,54 @@ class User
 
   public function getData(){
     $data = self::getUserData($this->id);
+  }
+
+  public function verifyUser(){
+    global $conn;
+    $sql = $conn->prepare("UPDATE users SET status = 1 WHERE id = ?");
+    $sql->bind_param('s', $this->id);
+    $sql->execute();
+  }
+
+  public function verifyOtp($otp){
+    global $conn;
+    $ffo = array(
+      'status' => 0,
+      'msg' => 'Immature function termination',
+    );
+
+    $userData = $this->getData();
+    if($userData !== false ){
+      if(intval($userData['otpExpiry']) >= time()){
+        if(intval($otp) === intval($userData['otp'])){
+          $ffo = array(
+            'status' => 1,
+            'msg' => 'OTP Ok',
+          );
+        }
+        else{
+          $ffo = array(
+            'status' => 0,
+            'msg' => 'Wrong OTP',
+          );
+        }
+      }
+      else{
+        self::sendOtp($this->id);
+        $ffo = array(
+          'status' => 0,
+          'msg' => 'OTP has expired, new one was sent to your email address',
+        );
+      }
+    }
+    else{
+      $ffo = array(
+        'status' => 0,
+        'msg' => 'Invalid OTP',
+      );
+    }
+
+    return $ffo;
   }
 }
 
