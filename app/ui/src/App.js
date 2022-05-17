354d8b9cd5fbb58c277bf9574469282a5759f5b3
PeterNdomano <peterndomano@gmail.com>
2022-05-17 13:19:21 +0300

    Basic UI Initialization

--- app/ui/src/App.js
+++ app/ui/src/App.js
@@ -1,17 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+export default class App {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome dear user</h1>
+      </div>
+    )
+  }
 }
-
-export default App;
