fd2607cd192fa0c1264e829ab5a7bafe14429584
PeterNdomano <peterndomano@gmail.com>
2022-05-19 22:52:42 +0300

    UI 103

--- app/ui/src/main-tabs/CTab.js
+++ app/ui/src/main-tabs/CTab.js
@@ -8,7 +8,9 @@ export default class CTab extends Component {
   render() {
     return (
       <div className="CTab">
-
+       <h1>
+          hhhhhhh hhhhhhhhhhhhh ccccccccccccccccc bbbbbbbbbbbbb1 xxxxxxxxxxxxxxxx bbbbbbbbbbbbb vvvvvvvvv 
+       </h1>
       </div>
     )
   }
