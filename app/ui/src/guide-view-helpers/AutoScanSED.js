8f97db6ec16ebdeb9207f5c4ed1accb4c7334dbd
PeterNdomano <peterndomano@gmail.com>
2022-06-02 14:17:40 +0300

    Search engine recon #101

---
+++ app/ui/src/guide-view-helpers/AutoScanSED.js
@@ -0,0 +1,23 @@
+/**
+  For Automatic Scan in Search Engine Discovery
+  */
+
+import React, { Component } from 'react';
+
+export default class AutoScanSED extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1>Automatic SED Tools</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ app/ui/src/guide-view-helpers/ManualScanSED.js
@@ -0,0 +1,26 @@
+/**
+  For Manual Scan in Search Engine Discovery
+  */
+
+import React, { Component } from 'react';
+
+export default class ManualScanSED extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1>
+              Perform manual Search Engine Reconnaissance with tools and
+              Engines of your choice then write a short report on your discoveries
+            </h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
