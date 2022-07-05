f86f1259df8e8eafaec60e1c0fb102e5bea7be0e
PeterNdomano <peterndomano@gmail.com>
2022-07-05 10:55:37 +0300

    MAA

--- app/ui/src/guide-view-helpers/MAAGuidesHelp.js
+++ app/ui/src/guide-view-helpers/MAAGuidesHelp.js
@@ -15,38 +15,28 @@ export default function GuidesHelp(props){
       <div className="row">
         <div className="col-md-12">
           <h3 className="helpTitle">Intro:</h3>
-          There is nothing new under the sun, and nearly every web application that one may think of developing has already
-          been developed. With the vast number of free and Open Source software projects that are actively developed and
-          deployed around the world, it is very likely that an application security test will face a target that is entirely or partly
-          dependent on these well known applications or frameworks (e.g. WordPress, phpBB, Mediawiki, etc). Knowing the web
-          application components that are being tested significantly helps in the testing process and will also drastically reduce
-          the effort required during the test. These well known web applications have known HTML headers, cookies, and
-          directory structures that can be enumerated to identify the application. Most of the web frameworks have several
-          markers in those locations which help an attacker or tester to recognize them. This is basically what all automatic tools
-          do, they look for a marker from a predefined location and then compare it to the database of known signatures. For
-          better accuracy several markers are usually used
+          The complexity of interconnected and heterogeneous web infrastructure can include hundreds of web applications and
+          makes configuration management and review a fundamental step in testing and deploying every single application. In
+          fact it takes only a single vulnerability to undermine the security of the entire infrastructure, and even small and
+          seemingly unimportant problems may evolve into severe risks for another application in the same infrastructure.
+          To address these problems, it is of utmost importance to perform an in-depth review of configuration and known security
+          issues. Before performing an in-depth review it is necessary to map the network and application architecture. The
+          different elements that make up the infrastructure need to be determined to understand how they interact with a web
+          application and how they affect security
           <hr/>
         </div>
 
         <div className="col-md-12">
           <h3 className="helpTitle">Tips & How:</h3>
-          <ul>
-            <li>
-              Fingerprint the components being used by the web applications
-            </li>
-            <li>
-              There are several common locations to consider in order to identify frameworks or components:
-              <ol>
-                <li>HTTP headers</li>
-                <li>Cookies</li>
-                <li>HTML source code</li>
-                <li>Specific files and folders</li>
-                <li>File extensions</li>
-                <li>Error messages</li>
-              </ol>
-            </li>
-
-          </ul>
+          The application architecture needs to be mapped through some test to determine what different components are used
+          to build the web application. In small setups, such as a simple PHP application, a single server might be used that
+          serves the PHP application, and perhaps also the authentication mechanism.
+          On more complex setups, such as an online bank system, multiple servers might be involved. These may include a
+          reverse proxy, a front-end web server, an application server, and a database server or LDAP server. Each of these
+          servers will be used for different purposes and might even be segregated in different networks with firewalls between
+          them. This creates different network zones so that access to the web server will not necessarily grant a remote user
+          access to the authentication mechanism itself, and so that compromises of the different elements of the architecture can
+          be isolated so that they will not compromise the whole architecture.
           <hr/>
         </div>
 
@@ -54,10 +44,10 @@ export default function GuidesHelp(props){
           <h3 className="helpTitle">Tools:</h3>
           <ul>
             <li>
-              WhatWeb <span className="linkText">https://github.com/urbanadventurer/WhatWeb</span>
+              Browsers (Chrome, firefox)
             </li>
             <li>
-              Wappalyzer <span className="linkText">https://www.wappalyzer.com/</span>
+              Burp suite, ZAP
             </li>
           </ul>
           <hr/>
