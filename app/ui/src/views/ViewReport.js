b2441a99f8f9972f934d3a2299578d49b31f288c
PeterNdomano <peterndomano@gmail.com>
2022-10-08 14:29:24 +0300

    reporting mods

--- app/ui/src/views/ViewReport.js
+++ app/ui/src/views/ViewReport.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
 import { tellUser, sanitizePathString, getInlineLoader, sanitizeUrl } from '../Helper';
 const path = window.require('path');
 const fs = window.require('fs');
@@ -17,7 +18,7 @@ export default class NewProject extends Component {
     return new Promise(resolve => {
       props.viliop.guide.menu.forEach((item, i) => {
         item.sections.forEach(async (item, i) => {
-          await props.viliop.currentProject.getReport(item.id).then(content => {
+          await props.viliop.currentProject.getReport(item.id, true).then(content => {
             this.setState((prevState) => {
               return {
                 reportMds: {
@@ -59,6 +60,11 @@ export default class NewProject extends Component {
         <div className="row">
           <div className="col-md-12">
             <h1 className="panelTitle font-light">Pentesting Report</h1>
+            <h6 className="font-right text-muted">Project Type: &nbsp;<span className="text-success font-mono">{(this.props.viliop.currentProject.config.type === "webPentest") ? "Website Pentesting" : "Mobile App Pentesting"}</span></h6>
+            <h6 className="font-right text-muted">Target:&nbsp; <span className="text-success font-mono">{this.props.viliop.currentProject.config.targetUrl}</span></h6>
+            <h6 className="font-right text-muted">Pentester's Name:&nbsp; <span className="text-success font-mono">Peter Ndomano</span></h6>
+            <h6 className="font-right text-muted">Pentester's Email:&nbsp; <span className="text-success font-mono">peterndomano@gmail.com</span></h6>
+            <h6 className="font-right text-muted">Created/Updated on:&nbsp; <span className="text-success font-mono">{ new Date().toDateString() }</span></h6>
           </div>
           {
             (this.props.viliop.currentProject === null) ?
@@ -68,20 +74,20 @@ export default class NewProject extends Component {
             (this.state.ready) ?
             this.props.viliop.guide.menu.map((item, i) => {
               return (
-                <div key={i} className="col-md-12" style={{ marginBottom:"20px" }}>
-                  <h3>{item.title}</h3>
-                  <blockquote className="reportBlock">
+                <div key={i} className="col-md-12" style={{ marginTop:"30px" }}>
+                  <h2 className="font-light text-primary">{item.title}</h2>
+                  <ol className="reportBlock">
                   {
                     item.sections.map((item, i) => {
                       return (
-                        <div key={i} className="text-left" style={{ marginBottom:"10px" }}>
-                          <h5>{item.title}</h5>
-                          {this.state.reportMds[item.id]}
-                        </div>
+                        <li key={i} className="text-left" style={{ marginTop:"20px" }}>
+                          <h5 className="font-bold text-success">{item.title}</h5>
+                          <ReactMarkdown>{this.state.reportMds[item.id]}</ReactMarkdown>
+                        </li>
                       )
                     })
                   }
-                  </blockquote>
+                  </ol>
                 </div>
               )
             })
