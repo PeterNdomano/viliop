cbca33dceac6ad4ae491c55124d3aafccfac3c20
PeterNdomano <peterndomano@gmail.com>
2022-10-14 14:20:30 +0300

    mods

--- app/ui/src/views/InstalledTools.js
+++ app/ui/src/views/InstalledTools.js
@@ -1,26 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import OneTool from '../ones/OneTool';
 import { tellUser, sanitizePathString, getInlineLoader, sanitizeUrl } from '../Helper';
 const path = window.require('path');
 const fs = window.require('fs');
 
 
-export default class EditReport extends Component {
-  constructor(props) {
-    super(props);
+export default function InstalledTools(props) {
+  const [ installedTools, setInstalledTools ] = useState(null);
+  const [ loading, setLoading ] = useState(true);
 
+  const init = async () => {
+    setLoading(true);
+    await props.viliop.getInstalledTools().then(tools => {
+      setInstalledTools(tools);
+      setLoading(false);
+    });
   }
 
+  useEffect(() => {
+    init();
+  }, [])
 
-
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <h1 className="panelTitle">Installed Tools</h1>
-          </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className="panelTitle">Installed Tools</h1>
         </div>
+        {
+          (loading) ?
+          <div className="col-md-12" style={{ height:"100px", position:"relative" }}>
+            {getInlineLoader()}
+          </div> :
+          <>
+            <div className="col-md-12">
+              {
+                (installedTools !== null && installedTools.length > 0) ?
+                installedTools.map((item, i) => {
+                  return <OneTool viliop={props.viliop} parent="installed" setLoading={setLoading} tool={item} key={i}/>
+                }):
+                <h6>No tools were found</h6>
+              }
+            </div>
+          </>
+        }
       </div>
-    )
-  }
+    </div>
+  )
 }
